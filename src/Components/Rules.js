c203424a1f8db255150583f5d6e1b133b1a589ce
jcmcarth06 <jcmcarth06@gmail.com>
2020-09-27 20:05:26 -0600

    update rules

--- src/Components/Rules.js
+++ src/Components/Rules.js
@@ -6,12 +6,12 @@ const Rules = () => {
             <div>
                 <h2>Rules of the Game:</h2>
                     <p>-Decide whether you want a one-syllable puzzle (Hink Pink), a two-syllable puzzle (Hinky Pinky), or a three-syllable puzzle (Hinkety Pinkety)</p>
-                    <p>The number of syllables in the title of the category indicates the number of syllables in the solution</p>
-                    <p>Each puzzle has a solution comprised of two rhyming words</p>
-                    <p>Each puzzle comes with a definition clue to the solution that fits within the syllable count of the category</p>
-                    <p>Example:</p>
+                    <p>-The number of syllables in the title of the category indicates the number of syllables in the solution</p>
+                    <p>-Each puzzle has a solution comprised of two rhyming words</p>
+                    <p>-Each puzzle comes with a definition clue to the solution that fits within the syllable count of the category</p>
+                    <p><b>Example:</b></p>
                     <p>HINKY PINKY// A filthy, small, flying animal</p>
-                    <p>This puzzle will have a solution comprised of two two-syllable words that rhyme with one another</p>
+                    <p>-This puzzle will have a solution comprised of two two-syllable words that rhyme with one another</p>
                     <p>(Solution: Dirty Birdy)</p>
             </div>
         )
